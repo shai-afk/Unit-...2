a4f5fa1ef5859fb5bbbbd278ce61ffe6dc337f56
Eugene Roberts <eugenerobertsokumu@gmail.com>
2026-09-16 16:11:06 +0300

    Add files via upload

---
+++ Units offered.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    // Name:Eugene Roberts
+    // Registration Number :CT100/G/30688/26
+    
+    printf("a. units offered this semester\n");
+    printf("b. introduction to programing and algorithms\n");
+    printf("c. data structure\n");
+    printf("d. database management system\n");
+    printf("e. software engineering\n");
+    return 0;
+}
