5a91e3f6723f85cf85f42f61063bbfec706a5538
Eugene Roberts <eugenerobertsokumu@gmail.com>
2026-09-25 09:18:42 +0300

    Add files via upload

---
+++ surface area and volume .c
@@ -0,0 +1,27 @@
+//Name:Eugene Roberts 
+//Reg Number:CT100/G/30688
+//variables and data types
+  #include<stdio.h>
+  
+  int main () {
+  float radius;
+  float height 
+  double pi =3.142 ;
+  float surface area
+  float volume 
+  
+  printf("input radius:\t");
+  scanf("%",&radius);
+  
+  printf("input height:\t");
+  scanf("%",&height);
+  
+  surface area =2×pi×radius×radius+2×pi×radius ×height;
+  volume=pi×radius×radius×height;
+  
+  printf("find volume:\t%f\n",volume);
+  printf("find surface area:\t%f\n" surface area);
+  
+  return 0;
+  
+  }
